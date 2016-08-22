17358cee3593cf9acbd8eb6366cdcaf9d9044a8e
yunhua <421747209@qq.com>
2016-08-22 21:36:59 +0800

    fist submin

---
+++ src/test/java/com/yunhua/TestHelloWorld.java
@@ -0,0 +1,15 @@
+package com.yunhua;
+
+import org.junit.*;
+import org.junit.Assert.*;
+
+public class TestHelloWorld{
+
+	@Test
+	public void testHello(){
+
+		Assert.assertEquals("Hello World!",new HelloWorld().sayHello());
+	}
+
+
+}
